5ddc36fbd69f3e5b2737f5e4434ded3a5a3a2367
DarkPred <26512194+darkpred@users.noreply.github.com>
2020-07-12 16:40:00 +0200

    code cleanup and multiplayer compatibility

--- src/main/java/darkpred/nocreativedrift/NoCreativeDrift.java
+++ src/main/java/darkpred/nocreativedrift/NoCreativeDrift.java
@@ -1,13 +1,19 @@
 package darkpred.nocreativedrift;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(NoCreativeDrift.MOD_ID)
 public class NoCreativeDrift {
 	public static final String MOD_ID = "nocreativedrift";
 	
 	public NoCreativeDrift() {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 }
--- src/main/java/darkpred/nocreativedrift/client/ClientEventHandler.java
+++ src/main/java/darkpred/nocreativedrift/client/ClientEventHandler.java
@@ -8,26 +8,28 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class ClientEventHandler {
 
+	@SuppressWarnings("unused")
 	private static final Logger LOGGER = LogManager.getLogger(ClientEventHandler.class);
 
 	@SubscribeEvent
 	public static void onPlayerTickEvent(PlayerTickEvent event) {
-		Minecraft mc = Minecraft.getInstance();
-		if (event.phase == Phase.END && mc.player != null) {
-			//Making sure that the player is creative flying
-			if (mc.player.isCreative() && mc.player.abilities.isFlying && !mc.player.isElytraFlying()) {
-				//False if any horizontal movement key is being pressed
+		//Ensures that the code is only run once on the logical client
+		if (event.phase == Phase.END && event.side == LogicalSide.CLIENT) {
+			// Making sure that the player is creative flying
+			if (event.player.isCreative() && event.player.abilities.isFlying && !event.player.isElytraFlying()) {
+				Minecraft mc = Minecraft.getInstance();
+				// False if any horizontal movement key is being pressed
 				if (!(mc.gameSettings.keyBindForward.isKeyDown() || mc.gameSettings.keyBindBack.isKeyDown() || mc.gameSettings.keyBindLeft.isKeyDown() || mc.gameSettings.keyBindRight.isKeyDown())) {
-					//Sets the players horizontal motion to 0
-					mc.player.setMotion(0, mc.player.getMotion().getY(), 0);
+					// Sets the players horizontal motion to 0
+					event.player.setMotion(0, event.player.getMotion().getY(), 0);
 				}
 			}
 		}
-		
 	}
 }
