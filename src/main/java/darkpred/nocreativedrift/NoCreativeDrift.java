bb66fd776b9b0aa1c1dfe247972cd99a18e14338
DarkPred <26512194+darkpred@users.noreply.github.com>
2021-03-26 22:29:51 +0100

    Add simply jetpacks compatibility and jetpack config

--- src/main/java/darkpred/nocreativedrift/NoCreativeDrift.java
+++ src/main/java/darkpred/nocreativedrift/NoCreativeDrift.java
@@ -1,19 +1,31 @@
 package darkpred.nocreativedrift;
 
-import org.apache.commons.lang3.tuple.Pair;
-
+import darkpred.nocreativedrift.config.ClientConfig;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod(NoCreativeDrift.MOD_ID)
 public class NoCreativeDrift {
-	public static final String MOD_ID = "nocreativedrift";
-	
-	public NoCreativeDrift() {
-		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
-		MinecraftForge.EVENT_BUS.register(this);
-	}
+    public static final String MOD_ID = "nocreativedrift";
+    private static boolean simplyJetpacksLoaded = false;
+
+    public NoCreativeDrift() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
+                () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ClientConfig.SPEC);
+        if (ModList.get().getModFileById("simplyjetpacks") != null) {
+            simplyJetpacksLoaded = true;
+        }
+    }
+
+    public static boolean isSimplyJetpacksLoaded() {
+        return simplyJetpacksLoaded;
+    }
 }
--- src/main/java/darkpred/nocreativedrift/client/ClientEventHandler.java
+++ src/main/java/darkpred/nocreativedrift/client/ClientEventHandler.java
@@ -1,35 +1,48 @@
 package darkpred.nocreativedrift.client;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import darkpred.nocreativedrift.NoCreativeDrift;
+import darkpred.nocreativedrift.config.ClientConfig;
 import net.minecraft.client.Minecraft;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
+import stormedpanda.simplyjetpacks.items.JetpackItem;
 
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class ClientEventHandler {
 
-	@SuppressWarnings("unused")
-	private static final Logger LOGGER = LogManager.getLogger(ClientEventHandler.class);
+    @SubscribeEvent
+    public static void onPlayerTickEvent(PlayerTickEvent event) {
+        //Ensures that the code is only run once on the logical client
+        if (event.phase == Phase.END && event.side == LogicalSide.CLIENT) {
+            // Making sure that the player is creative flying
+            if (event.player.isCreative() && event.player.abilities.isFlying && !event.player.isElytraFlying()) {
+                stopFlight(event);
+            }
+            if (!ClientConfig.disableJetpackDrift.get()) {
+                ItemStack itemStack = event.player.getItemStackFromSlot(EquipmentSlotType.CHEST);
+                if (NoCreativeDrift.isSimplyJetpacksLoaded()) {
+                    if (itemStack.getItem().getClass() == JetpackItem.class) {
+                        if (((JetpackItem) itemStack.getItem()).isEngineOn(itemStack)) {
+                            stopFlight(event);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-	@SubscribeEvent
-	public static void onPlayerTickEvent(PlayerTickEvent event) {
-		//Ensures that the code is only run once on the logical client
-		if (event.phase == Phase.END && event.side == LogicalSide.CLIENT) {
-			// Making sure that the player is creative flying
-			if (event.player.isCreative() && event.player.abilities.isFlying && !event.player.isElytraFlying()) {
-				Minecraft mc = Minecraft.getInstance();
-				// False if any horizontal movement key is being pressed
-				if (!(mc.gameSettings.keyBindForward.isKeyDown() || mc.gameSettings.keyBindBack.isKeyDown() || mc.gameSettings.keyBindLeft.isKeyDown() || mc.gameSettings.keyBindRight.isKeyDown())) {
-					// Sets the players horizontal motion to 0
-					event.player.setMotion(0, event.player.getMotion().getY(), 0);
-				}
-			}
-		}
-	}
+    private static void stopFlight(PlayerTickEvent event) {
+        Minecraft mc = Minecraft.getInstance();
+        // False if any horizontal movement key is being pressed
+        if (!(mc.gameSettings.keyBindForward.isKeyDown() || mc.gameSettings.keyBindBack.isKeyDown() || mc.gameSettings.keyBindLeft.isKeyDown() || mc.gameSettings.keyBindRight.isKeyDown())) {
+            // Sets the players horizontal motion to 0
+            event.player.setMotion(0, event.player.getMotion().getY(), 0);
+        }
+    }
 }
