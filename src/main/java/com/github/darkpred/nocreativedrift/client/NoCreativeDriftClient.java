7cd806668ad64f7024e47698667b0a15641941b4
DarkPred <cookie@exedra.de>
2021-04-06 17:22:02 +0200

    Set up fabric project

---
+++ src/main/java/com/github/darkpred/nocreativedrift/client/NoCreativeDriftClient.java
@@ -0,0 +1,22 @@
+package com.github.darkpred.nocreativedrift.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+
+public class NoCreativeDriftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            ClientPlayerEntity player = client.player;
+            if (player != null && player.abilities.flying) {
+                MinecraftClient mc = MinecraftClient.getInstance();
+                if (!(mc.options.keyForward.isPressed() || mc.options.keyBack.isPressed() || mc.options.keyLeft.isPressed() || mc.options.keyRight.isPressed())) {
+                    player.setVelocity(0, player.getVelocity().getY(), 0);
+                }
+            }
+        });
+    }
+}
