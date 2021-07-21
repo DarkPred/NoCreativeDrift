c974c02ec4d8c7b40483649946daf88a2cd30887
DarkPred <cookie@exedra.de>
2021-07-21 20:41:46 +0200

    Add config for vertical drift

--- src/main/java/com/github/darkpred/nocreativedrift/client/NoCreativeDriftClient.java
+++ src/main/java/com/github/darkpred/nocreativedrift/client/NoCreativeDriftClient.java
@@ -4,8 +4,15 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.util.math.Vec3d;
 
 public class NoCreativeDriftClient implements ClientModInitializer {
+
+    private boolean keyJumpPressed = false;
+    private boolean keySneakPressed = false;
+    private final SimpleConfig config = SimpleConfig.of("nocreativedrift").provider(namespace -> {
+        return "#Disable the drift during vertical flight\ndisableVerticalDrift=false";
+    }).request();
 
     @Override
     public void onInitializeClient() {
@@ -20,8 +27,23 @@ public class NoCreativeDriftClient implements ClientModInitializer {
 
     private void stopDrift(ClientPlayerEntity player) {
         MinecraftClient mc = MinecraftClient.getInstance();
+        Vec3d velocity = player.getVelocity();
         if (!(mc.options.keyForward.isPressed() || mc.options.keyBack.isPressed() || mc.options.keyLeft.isPressed() || mc.options.keyRight.isPressed())) {
-            player.setVelocity(0, player.getVelocity().getY(), 0);
+            player.setVelocity(0, velocity.getY(), 0);
+        }
+        if (config.getOrDefault("disableVerticalDrift", false)) {
+            if (keyJumpPressed && !mc.options.keyJump.isPressed()) {
+                player.setVelocity(velocity.getX(), 0, velocity.getZ());
+                keyJumpPressed = false;
+            } else if (mc.options.keyJump.isPressed()) {
+                keyJumpPressed = true;
+            }
+            if (keySneakPressed && !mc.options.keySneak.isPressed()) {
+                player.setVelocity(velocity.getX(), 0, velocity.getZ());
+                keySneakPressed = false;
+            } else if (mc.options.keySneak.isPressed()) {
+                keySneakPressed = true;
+            }
         }
     }
 }
