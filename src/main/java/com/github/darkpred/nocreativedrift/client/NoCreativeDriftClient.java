04a4f78c09f1b99eff72acf8c979c1a80431ae53
DarkPred <26512194+darkpred@users.noreply.github.com>
2021-08-29 03:50:57 +0200

    Add config for vertical drift and jetpack drift

--- src/main/java/com/github/darkpred/nocreativedrift/client/NoCreativeDriftClient.java
+++ src/main/java/com/github/darkpred/nocreativedrift/client/NoCreativeDriftClient.java
@@ -8,9 +8,17 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Vec3d;
 import team.reborn.energy.Energy;
 
 public class NoCreativeDriftClient implements ClientModInitializer {
+
+    private final SimpleConfig config = SimpleConfig.of("nocreativedrift").provider(namespace ->
+            "#Disable the drift during vertical flight\ndisableVerticalDrift=false\n" +
+                    "#Disable the drift on jetpacks\ndisableJetpackDrift=false\n"
+    ).request();
+    private boolean keyJumpPressed = false;
+    private boolean keySneakPressed = false;
 
     private static boolean isEngineOn(ItemStack itemStack) {
         return itemStack.getTag() != null && itemStack.getTag().getBoolean("Engine");
@@ -25,7 +33,7 @@ public class NoCreativeDriftClient implements ClientModInitializer {
             if (player.abilities.flying) {
                 stopDrift(player);
             }
-            if (ironJetpacksLoaded) {
+            if (ironJetpacksLoaded && config.getOrDefault("disableJetpackDrift", false)) {
                 ItemStack itemStack = player.getEquippedStack(EquipmentSlot.CHEST);
                 if (itemStack.getItem() instanceof JetpackItem && isEngineOn(itemStack)) {
                     if (Energy.of(itemStack).getEnergy() > 0) {
@@ -38,8 +46,23 @@ public class NoCreativeDriftClient implements ClientModInitializer {
 
     private void stopDrift(ClientPlayerEntity player) {
         MinecraftClient mc = MinecraftClient.getInstance();
+        Vec3d velocity = player.getVelocity();
         if (!(mc.options.keyForward.isPressed() || mc.options.keyBack.isPressed() || mc.options.keyLeft.isPressed() || mc.options.keyRight.isPressed())) {
-            player.setVelocity(0, player.getVelocity().getY(), 0);
+            player.setVelocity(0, velocity.getY(), 0);
+        }
+        if (config.getOrDefault("disableVerticalDrift", false)) {
+            if (keyJumpPressed && !mc.options.keyJump.isPressed()) {
+                player.setVelocity(velocity.getX(), 0, velocity.getZ());
+                keyJumpPressed = false;
+            } else if (mc.options.keyJump.isPressed()) {
+                keyJumpPressed = true;
+            }
+            if (keySneakPressed && !mc.options.keySneak.isPressed()) {
+                player.setVelocity(velocity.getX(), 0, velocity.getZ());
+                keySneakPressed = false;
+            } else if (mc.options.keySneak.isPressed()) {
+                keySneakPressed = true;
+            }
         }
     }
 }
