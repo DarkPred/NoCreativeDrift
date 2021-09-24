e52718ca7d6330f6fc724ac649a9a5af0c23b857
DarkPred <cookie@exedra.de>
2021-09-24 22:38:48 +0200

    Add fading hud message

--- src/main/java/com/github/darkpred/nocreativedrift/client/NoCreativeDriftClient.java
+++ src/main/java/com/github/darkpred/nocreativedrift/client/NoCreativeDriftClient.java
@@ -16,41 +16,39 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class NoCreativeDriftClient implements ClientModInitializer {
-    private static final Deque<Drift> DRIFT = new ArrayDeque<>();
     private static final KeyBinding toggleDrift = new KeyBinding(
             "key.nocreativedrift.toggle_drift", GLFW.GLFW_KEY_C, "No Creative Drift");
-
-    static {
-        DRIFT.add(Drift.VANILLA);
-        DRIFT.add(Drift.STRONG);
-        DRIFT.add(Drift.WEAK);
-        DRIFT.add(Drift.DISABLED);
-    }
-
+    private final Deque<Drift> driftQueue = new ArrayDeque<>();
     private boolean keyJumpPressed = false;
     private boolean keySneakPressed = false;
     private boolean keyToggleDriftPressed = false;
-
-    private static Drift curDrift() {
-        Drift ret = DRIFT.peek();
-        if (ret == null) {
-            ret = Drift.DISABLED;
-        }
-        return ret;
-    }
+    private float opacity = 5.0f;
 
     @Override
     public void onInitializeClient() {
+        //Init drift strength order
+        int curDrift = ClientConfig.CONFIG.getOrDefault("driftStrength", 3);
+        Drift[] drifts = Drift.values();
+        while (driftQueue.size() < drifts.length) {
+            driftQueue.add(drifts[curDrift % 4]);
+            curDrift++;
+        }
+
         if (ClientConfig.CONFIG.getOrDefault("enableToggleKeyBind", false)) {
             KeyBindingHelper.registerKeyBinding(toggleDrift);
         }
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             ClientPlayerEntity player = client.player;
-            if (player == null) return;
+            if (player == null) {
+                opacity = 5;
+                return;
+            }
+            opacity = Math.max(opacity - 0.05f, 0);
             if (toggleDrift.isPressed() != keyToggleDriftPressed) {
                 if (!keyToggleDriftPressed) {
-                    DRIFT.add(DRIFT.pop());
-                    ClientConfig.CONFIG.set("driftStrength", DRIFT.peek().ordinal());
+                    driftQueue.add(driftQueue.pop());
+                    ClientConfig.CONFIG.set("driftStrength", driftQueue.peek().ordinal());
+                    opacity = 5.0f;
                 }
                 keyToggleDriftPressed = toggleDrift.isPressed();
             }
@@ -60,20 +58,34 @@ public class NoCreativeDriftClient implements ClientModInitializer {
         });
 
         HudRenderCallback.EVENT.register((matrixStack, tickDelta) -> {
+            if (ClientConfig.CONFIG.getOrDefault("enableHudFading", false)) {
+                if (opacity <= 0) {
+                    return;
+                }
+            }
             if (ClientConfig.CONFIG.getOrDefault("enableHudMessage", false)) {
                 MinecraftClient mc = MinecraftClient.getInstance();
                 float yPosition = (float) (0.3 * mc.getWindow().getScaledHeight());
-                TranslatableText text = new TranslatableText("hud.nocreativedrift.drift_strength", curDrift().getText());
-                mc.textRenderer.drawWithShadow(matrixStack, text, 2, yPosition, 0xEEEBF0);
+                int color = addOpacityToColor(opacity, "EEEBF0");
+                TranslatableText text = new TranslatableText("hud.nocreativedrift.drift_strength", getCurDrift().getText());
+                mc.textRenderer.drawWithShadow(matrixStack, text, 2, yPosition, color);
             }
         });
+    }
+
+    /**
+     * Adds opacity to a given hex color. Does not work with opacity of 0
+     */
+    private int addOpacityToColor(float opacity, String hexColor) {
+        opacity = Math.min(opacity, 1);
+        return Integer.parseUnsignedInt(Integer.toHexString((int) (opacity * 255)) + hexColor, 16);
     }
 
     private void stopHorizontalDrift(ClientPlayerEntity player) {
         MinecraftClient mc = MinecraftClient.getInstance();
         Vec3d velocity = player.getVelocity();
         if (!(mc.options.keyForward.isPressed() || mc.options.keyBack.isPressed() || mc.options.keyLeft.isPressed() || mc.options.keyRight.isPressed())) {
-            player.setVelocity(velocity.getX() * curDrift().getMulti(), velocity.getY(), velocity.getZ() * curDrift().getMulti());
+            player.setVelocity(velocity.getX() * getCurDrift().getMulti(), velocity.getY(), velocity.getZ() * getCurDrift().getMulti());
         }
     }
 
@@ -88,17 +100,25 @@ public class NoCreativeDriftClient implements ClientModInitializer {
         if (ClientConfig.CONFIG.getOrDefault("disableVerticalDrift", false)) {
             if (keyJumpPressed && !mc.options.keyJump.isPressed()) {
                 //Multiplier only applied once but that's fine because there is barely no drift anyway
-                player.setVelocity(velocity.getX(), velocity.getY() * curDrift().getMulti(), velocity.getZ());
+                player.setVelocity(velocity.getX(), velocity.getY() * getCurDrift().getMulti(), velocity.getZ());
                 keyJumpPressed = false;
             } else if (mc.options.keyJump.isPressed()) {
                 keyJumpPressed = true;
             }
             if (keySneakPressed && !mc.options.keySneak.isPressed()) {
-                player.setVelocity(velocity.getX(), velocity.getY() * curDrift().getMulti(), velocity.getZ());
+                player.setVelocity(velocity.getX(), velocity.getY() * getCurDrift().getMulti(), velocity.getZ());
                 keySneakPressed = false;
             } else if (mc.options.keySneak.isPressed()) {
                 keySneakPressed = true;
             }
         }
+    }
+
+    private Drift getCurDrift() {
+        Drift ret = driftQueue.peek();
+        if (ret == null) {
+            ret = Drift.DISABLED;
+        }
+        return ret;
     }
 }
