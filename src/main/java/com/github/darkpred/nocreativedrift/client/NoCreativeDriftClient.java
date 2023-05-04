355b9b344bb24a1448879dbdf80514a5ba6b813f
DarkPred <26512194+darkpred@users.noreply.github.com>
2023-05-04 23:16:06 +0200

    Add support for InvMove

--- src/main/java/com/github/darkpred/nocreativedrift/client/NoCreativeDriftClient.java
+++ src/main/java/com/github/darkpred/nocreativedrift/client/NoCreativeDriftClient.java
@@ -1,12 +1,15 @@
 package com.github.darkpred.nocreativedrift.client;
 
 import com.github.darkpred.nocreativedrift.client.config.ClientConfig;
+import me.pieking1215.invmove.InvMove;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
@@ -83,9 +86,9 @@ public class NoCreativeDriftClient implements ClientModInitializer {
     }
 
     private void stopHorizontalDrift(ClientPlayerEntity player) {
-        MinecraftClient mc = MinecraftClient.getInstance();
+        GameOptions opt = MinecraftClient.getInstance().options;
         Vec3d velocity = player.getVelocity();
-        if (!(mc.options.forwardKey.isPressed() || mc.options.backKey.isPressed() || mc.options.leftKey.isPressed() || mc.options.rightKey.isPressed())) {
+        if (!(isKeyDown(opt.forwardKey) || isKeyDown(opt.backKey) || isKeyDown(opt.leftKey) || isKeyDown(opt.rightKey))) {
             player.setVelocity(velocity.getX() * getCurDrift().getMulti(), velocity.getY(), velocity.getZ() * getCurDrift().getMulti());
         }
     }
@@ -94,24 +97,32 @@ public class NoCreativeDriftClient implements ClientModInitializer {
      * Currently not used for jetpacks
      */
     private void stopDrift(ClientPlayerEntity player) {
-        MinecraftClient mc = MinecraftClient.getInstance();
+        GameOptions options = MinecraftClient.getInstance().options;
         Vec3d velocity = player.getVelocity();
         //If no movement keys are pressed slow down player. Seems to work fine with pistons and stuff
         stopHorizontalDrift(player);
         if (ClientConfig.CONFIG.getOrDefault("disableVerticalDrift", false)) {
-            if (keyJumpPressed && !mc.options.jumpKey.isPressed()) {
+            if (keyJumpPressed && !isKeyDown(options.jumpKey)) {
                 //Multiplier only applied once but that's fine because there is barely no drift anyway
                 player.setVelocity(velocity.getX(), velocity.getY() * getCurDrift().getMulti(), velocity.getZ());
                 keyJumpPressed = false;
-            } else if (mc.options.jumpKey.isPressed()) {
+            } else if (isKeyDown(options.jumpKey)) {
                 keyJumpPressed = true;
             }
-            if (keySneakPressed && !mc.options.sneakKey.isPressed()) {
+            if (keySneakPressed && !isKeyDown(options.sneakKey)) {
                 player.setVelocity(velocity.getX(), velocity.getY() * getCurDrift().getMulti(), velocity.getZ());
                 keySneakPressed = false;
-            } else if (mc.options.sneakKey.isPressed()) {
+            } else if (isKeyDown(options.sneakKey)) {
                 keySneakPressed = true;
             }
+        }
+    }
+
+    public static boolean isKeyDown(KeyBinding key) {
+        if (FabricLoader.getInstance().isModLoaded("invmove")) {
+            return InvMove.rawIsKeyDown(key);
+        } else {
+            return key.isPressed();
         }
     }
 
