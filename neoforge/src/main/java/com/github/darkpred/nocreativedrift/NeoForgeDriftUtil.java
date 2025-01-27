71ba3980160da250bb3c338d4d9030501ee403c5
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-01-27 13:44:19 +0100

    Fix midnightlib version and remove jetpack mods from dependencies

--- neoforge/src/main/java/com/github/darkpred/nocreativedrift/NeoForgeDriftUtil.java
+++ neoforge/src/main/java/com/github/darkpred/nocreativedrift/NeoForgeDriftUtil.java
@@ -1,10 +1,7 @@
 package com.github.darkpred.nocreativedrift;
 
-import com.blakebr0.ironjetpacks.util.JetpackUtils;
 import com.google.auto.service.AutoService;
-import mekanism.common.item.interfaces.IJetpackItem;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.neoforged.fml.ModList;
 
 @AutoService(DriftUtil.class)
